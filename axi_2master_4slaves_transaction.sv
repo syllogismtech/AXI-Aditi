4387da43fc0a05c08d574e6f831760e4b3d4a4c1
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-15 13:43:03 +0530

    axi_2master_4slaves_transaction.sv 1st commit

---
+++ axi_2master_4slaves_transaction.sv
@@ -0,0 +1,15 @@
+class axi_transaction extends uvm_sequence_item;
+  rand bit is_read;
+  rand bit [3:0] id;
+  rand bit [31:0] addr;
+  rand bit [3:0] len;
+  rand bit [2:0] size;
+  rand bit [1:0] burst;
+  rand bit [31:0] data[];
+
+  `uvm_object_utils(axi_transaction)
+
+  function new(string name = "axi_transaction");
+    super.new(name);
+  endfunction
+endclass
